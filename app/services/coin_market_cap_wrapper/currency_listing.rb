f420beaea9f18af933f511b00aa2a3c0feed2c2a
flymypie <flymaipie@gmail.com>
2018-12-26 23:58:16 +0000

    make services and worker work properly

--- app/services/coin_market_cap_wrapper/currency_listing.rb
+++ app/services/coin_market_cap_wrapper/currency_listing.rb
@@ -2,13 +2,13 @@ class CoinMarketCapWrapper::CurrencyListing
   require 'httparty'
   class << self
     def init_data
-      url = 'https://api.coinmarketcap.com/v2/listings/'
+      url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
       response = HTTParty.get(url, headers: {'X-CMC_PRO_API_KEY' => ENV["API_KEY"]})
       JSON.parse(response.body)
     end
     def save_data_in_redis
       init_data['data'].each do |currency|
-        REDIS.set(currency['name'], currency)
+        REDIS.set(currency['symbol'], currency.to_json)
       end
     end
   end
--- app/workers/update_currency_worker.rb
+++ app/workers/update_currency_worker.rb
@@ -4,12 +4,5 @@ class UpdateCurrencyWorker
 
   def perform
     CoinMarketCapWrapper::CurrencyListing.save_data_in_redis
-    list_of_key = REDIS.keys
-    list_of_key.each do |key|
-      coin_data = CoinMarketCapWrapper::CurrencyInfo.get_data(key)['data']
-      if coin_data
-        REDIS.set(key, coin_data['quotes']['USD']['price'])
-      end
-    end
   end
 end
