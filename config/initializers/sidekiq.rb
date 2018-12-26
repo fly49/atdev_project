442018690102e83ffddeac4cb2324730bb41d9b1
flymypie <flymaipie@gmail.com>
2018-12-26 21:33:08 +0000

    run scheduled sidekiq worker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,5 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV["REDIS_URL"] }
 end
+
+Sidekiq::Cron::Job.create(name: 'UpdateCurrencyWorker - every 5min', cron: '*/5 * * * *', class: 'UpdateCurrencyWorker')
