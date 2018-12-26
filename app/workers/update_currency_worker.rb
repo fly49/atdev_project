035407c056feba1b8234e9640ced813fed970b14
flymypie <flymaipie@gmail.com>
2018-12-26 21:34:31 +0000

    use httparty in services

--- app/workers/update_currency_worker.rb
+++ app/workers/update_currency_worker.rb
@@ -1,15 +1,15 @@
 class UpdateCurrencyWorker
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
   def perform
+    CoinMarketCapWrapper::CurrencyListing.save_data_in_redis
     list_of_key = REDIS.keys
     list_of_key.each do |key|
-      if CoinMarketCapWrapper::CurrencyInfo.get_data(key)['data'] != nil
-        cost = CoinMarketCapWrapper::CurrencyInfo.get_data(key)['data']['quotes']
+      coin_data = CoinMarketCapWrapper::CurrencyInfo.get_data(key)['data']
+      if coin_data
+        REDIS.set(key, coin_data['quotes']['USD']['price'])
       end
-      cost.compact
-      REDIS.set(key, cost['USD']['price'])
     end
   end
-  UpdateCurrencyWorker.perform_at(5.minute)
 end
