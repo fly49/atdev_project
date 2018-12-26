db11d667b50ffeb3e4360c67ff3b391fd2a7f86a
flymypie <flymaipie@gmail.com>
2018-12-26 23:56:50 +0000

    change workers name and period

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,4 +6,4 @@ Sidekiq.configure_client do |config|
   config.redis = { url: ENV["REDIS_URL"] }
 end
 
-Sidekiq::Cron::Job.create(name: 'UpdateCurrencyWorker - every 5min', cron: '*/5 * * * *', class: 'UpdateCurrencyWorker')
+Sidekiq::Cron::Job.create(name: 'UpdateCurrency - every 1 hour', cron: '0 */2 * * *', class: 'UpdateCurrencyWorker')
